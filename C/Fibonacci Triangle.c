f1313c1d450a3d3459f0bf7f1a4603688f788fd9
jayarudran <56425573+jayarudran@users.noreply.github.com>
2022-10-12 06:14:28 +0530

    Update Fibonacci Triangle.c

--- C/Fibonacci Triangle.c
+++ C/Fibonacci Triangle.c
@@ -3,7 +3,7 @@
 int main(){  
    int a=0,b=1,i,c,n,j;    
 system("cls");  
-    printf("Enter the limit:");    
+    printf("Enter the limit o given triangle:");    
     scanf("%d",&n);    
     for(i=1;i<=n;i++)    
     {    
